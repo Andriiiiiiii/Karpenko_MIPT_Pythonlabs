561867ab1f807060fcaa4f4deb9da348cccf60d9
Andriiiiiiii <91418387+Andriiiiiiii@users.noreply.github.com>
2022-11-18 11:34:31 +0300

    Delete E.cpp

--- E.cpp
+++
@@ -1,9 +0,0 @@
-class Kwama: public Creature{
-    protected:
-    int age;
-    public:
-    Kwama (double x, double y, double vx, double vy, int age) : Creature(x, y, vx, vy), age(age) { }
-    virtual void act_like_a_kwama() = 0;
-    virtual void pouch() = 0;
-    virtual unsigned int slaughter() = 0;
-};
